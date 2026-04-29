3f0471d61b966216f4c36f058886f7f9079bef91
Hyuk <paxjc2000@snu.ac.kr>
2026-04-29 14:37:34 +0900

    refactor: 뉴스스탠드 컴포넌트 정리

---
+++ src/app/_components/newsstand/icons.tsx
@@ -0,0 +1,58 @@
+export function NewspaperIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M4 5.5h13.5v13H4z" />
+      <path d="M17.5 8H20v9a1.5 1.5 0 0 1-1.5 1.5h-1" />
+      <path d="M7 8.5h7.5M7 12h7.5M7 15.5h4" />
+    </svg>
+  );
+}
+
+export function ListIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M8 7h11M8 12h11M8 17h11" />
+      <path d="M4.5 7h.01M4.5 12h.01M4.5 17h.01" />
+    </svg>
+  );
+}
+
+export function GridIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M5 5h5.5v5.5H5zM13.5 5H19v5.5h-5.5zM5 13.5h5.5V19H5zM13.5 13.5H19V19h-5.5z" />
+    </svg>
+  );
+}
+
+export function ChevronLeftIcon() {
+  return (
+    <svg viewBox="0 0 24 40" aria-hidden="true">
+      <path d="m15 11-7 9 7 9" />
+    </svg>
+  );
+}
+
+export function ChevronRightIcon() {
+  return (
+    <svg viewBox="0 0 24 40" aria-hidden="true">
+      <path d="m9 11 7 9-7 9" />
+    </svg>
+  );
+}
+
+export function PlusIcon() {
+  return (
+    <svg viewBox="0 0 10 10" aria-hidden="true">
+      <path d="M5 1.5v7M1.5 5h7" />
+    </svg>
+  );
+}
+
+export function MinusIcon() {
+  return (
+    <svg viewBox="0 0 10 10" aria-hidden="true">
+      <path d="M1.5 5h7" />
+    </svg>
+  );
+}
